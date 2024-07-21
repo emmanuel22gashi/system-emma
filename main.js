e5cf648c6a169e15e97c251c48079392ff7bfe77
emmanuel22gashi <emmanuel.gashi22m@outlook.com>
2024-07-21 11:04:15 +0300

    system

---
+++ main.js
@@ -0,0 +1,17 @@
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let boii = document.getElementById("boii");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+
+   
+
+console.log(submit);
+
+
+
